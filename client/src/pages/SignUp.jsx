b967a1ac1c90b6f52aa4901355306f81d47e16a5
Abhinav Singh <singh111abhinav@gmail.com>
2024-01-29 00:20:08 +0530

    add proxy,complete sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 const SignUp = () => {
+  const [error , setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    const formData = {username:e.target[0].value,email:e.target[1].value,password:e.target[2].value}
+    setLoading(true)
+    try {
+      const res = await fetch('/api/auth/signup',{
+        method: 'POST',
+        headers:{'content-Type':'application/json'},
+        body:JSON.stringify(formData)
+      })
+      const data = await res.json();
+      if(data.success===false){
+        setLoading(false)
+        setError(data.message)
+        
+        return
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/signin');
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+      
+    }
+    
+    
+  }
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           className="border p-3 rounded-lg"
           placeholder="username"
@@ -19,8 +51,8 @@ const SignUp = () => {
           placeholder="password"
           id="password"
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -29,6 +61,7 @@ const SignUp = () => {
           <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 };
